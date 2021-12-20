11bfc1e233076eae6d5c5491949312b3ec49e6e3
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-12-20 02:20:40 +0100

    added jest, cypress GitHub Action

---
+++ tests/example.spec.js
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom/extend-expect";
+
+import { getByText } from "@testing-library/dom";
+
+import { printUsers } from "../src/index";
+
+test("examples of some things", async () => {
+    const container = await printUsers();
+    document.body.appendChild(container);
+
+    const user = getByText(containe, "Current users");
+
+    expect(user).toBeInTheDocument();
+});
